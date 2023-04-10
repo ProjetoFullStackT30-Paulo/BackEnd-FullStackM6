a2d71dbef860c53f46c9b29cb1fd400871838698
Lucas <lucassoares2007@hotmail.com>
2023-04-10 01:10:00 -0300

    refactor: app use handler error

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,22 @@
 import "express-async-errors";
 import express from "express";
 import { appRoutes } from "./router";
+import { errorHandler } from "./errors";
 
 const app = express();
 
 app.use(express.json());
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "*");
-    res.setHeader("Access-Control-Allow-Headers", "*");
-  
-    next();
-  });
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "*");
+  res.setHeader("Access-Control-Allow-Headers", "*");
+
+  next();
+});
 
 app.use(appRoutes);
+
+app.use(errorHandler);
 
 export default app;
