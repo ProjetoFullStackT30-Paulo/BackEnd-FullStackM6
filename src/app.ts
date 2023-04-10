eaad6bab1df02ae9f81d0d06b2d5fe9c3ac02891
Lucas <lucassoares2007@hotmail.com>
2023-04-10 01:07:52 -0300

    refactor: app config cors

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,17 @@ import express from "express";
 import { appRoutes } from "./router";
 
 const app = express();
+
 app.use(express.json());
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "*");
+    res.setHeader("Access-Control-Allow-Headers", "*");
+  
+    next();
+  });
+
 app.use(appRoutes);
 
 export default app;
