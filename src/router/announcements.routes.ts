d9b756968ced5db0d87da2cdced082d03cbd57cb
Lucas <lucassoares2007@hotmail.com>
2023-04-10 22:26:09 -0300

    refactor: announcements routes remove list with users

--- src/router/announcements.routes.ts
+++ src/router/announcements.routes.ts
@@ -3,7 +3,6 @@ import {
   createAnnouncementController,
   deleteAnnouncementController,
   listAnnouncementController,
-  listAnnouncementWithUserController,
   retrieveAnnouncementController,
   updateAnnouncementController,
 } from "../controllers";
@@ -12,7 +11,6 @@ export const announcementRouter = Router();
 
 announcementRouter.post("/:id", createAnnouncementController);
 announcementRouter.get("", listAnnouncementController);
-announcementRouter.get("/user/:id", listAnnouncementWithUserController);
 announcementRouter.get("/:id", retrieveAnnouncementController);
 announcementRouter.patch("/:id", updateAnnouncementController);
 announcementRouter.delete("/:id", deleteAnnouncementController);
