ecaa676a578ed8fb6c86efa65dc344b8ce915d87
Mário Lucas de Almeida Silva <mario.lucas250702@gmail.com>
2023-04-27 22:18:44 -0300

    feat: added resetPassword functions, libs to mail and refactored any functions.

--- src/router/announcements.routes.ts
+++ src/router/announcements.routes.ts
@@ -1,4 +1,6 @@
 import { Router } from "express";
+import { AnnouncementCreateSchema, AnnouncementUpdateSchema } from "../schemas";
+
 import {
   createAnnouncementController,
   deleteAnnouncementController,
@@ -6,13 +8,13 @@ import {
   retrieveAnnouncementController,
   updateAnnouncementController,
 } from "../controllers";
+
 import {
   validateSchemaMiddleware,
   verifyAnnouncementOwner,
   verifyIsSeller,
   verifyUserIsAuthenticated,
 } from "../middlewares";
-import { AnnouncementCreateSchema, AnnouncementUpdateSchema } from "../schemas";
 
 export const announcementRouter = Router();
 
