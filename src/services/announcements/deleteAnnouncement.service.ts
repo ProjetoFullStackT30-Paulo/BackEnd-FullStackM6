df5404b9220d46a03f8f57ae73bb34e342e1563f
Lucas <lucassoares2007@hotmail.com>
2023-05-04 01:33:12 -0300

    build: comments

--- src/services/announcements/deleteAnnouncement.service.ts
+++ src/services/announcements/deleteAnnouncement.service.ts
@@ -21,8 +21,7 @@ export const deleteAnnouncementService = async (id: string) => {
     }
     announcement.listImage.forEach(async ({ image }) => {
       const key = image.key;
-      await prisma.clear
-      image.delete({ where: { key } });
+      await prisma.image.delete({ where: { key } });
       if (!process.env.APP_URL) {
         await cloudinary.uploader.destroy(key);
       } else {
